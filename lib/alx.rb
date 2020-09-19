2a81902b85594e253305b359f6a6b7b41eeb8749
Taikocuya <taikocuya@gmail.com>
2020-09-20 01:58:46 +0200

    Added default library file alx.rb

---
+++ lib/alx.rb
@@ -0,0 +1,33 @@
+#******************************************************************************
+# ALX - Skies of Arcadia Legends Examiner
+# Copyright (C) 2020 Marcel Renner
+# 
+# This file is part of ALX.
+# 
+# ALX is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU General Public License as published by the Free Software 
+# Foundation, either version 3 of the License, or (at your option) any later 
+# version.
+# 
+# ALX is distributed in the hope that it will be useful, but WITHOUT ANY 
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+# details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# ALX. If not, see <http://www.gnu.org/licenses/>.
+#******************************************************************************
+
+#==============================================================================
+#                                 REQUIREMENTS
+#==============================================================================
+
+require_relative('alx/stdentrytransform.rb')
+require_relative('alx/stdentrydata.rb')
+require_relative('alx/stdentry.rb')
+
+Dir.glob(File.join(File.dirname(__FILE__), 'alx', '*.rb')).each do |_p|
+  unless File.directory?(_p)
+    require_relative(File.join('alx', File.basename(_p)))
+  end
+end
