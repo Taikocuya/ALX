1bcb88560fc90f5d98d9d694fcc8d5d323901ba7
Taikocuya <github-ox51@renner.bz>
2024-11-13 22:56:26 +0100

    Added a command line tool for checking game roots

---
+++ bin/checkroot.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/ruby
+#******************************************************************************
+# ALX - Skies of Arcadia Legends Examiner
+# Copyright (C) 2024 Marcel Renner
+# 
+# This file is part of ALX.
+# 
+# ALX is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU General Public License as published by the Free Software 
+# Foundation, either version 3 of the License, or (at your option) any later 
+# version.
+# 
+# ALX is distributed in the hope that it will be useful, but WITHOUT ANY 
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+# details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with ALX.  If not, see <http://www.gnu.org/licenses/>.
+#******************************************************************************
+
+#==============================================================================
+#                                 REQUIREMENTS
+#==============================================================================
+
+require_relative('../lib/alx/entrytransform.rb')
+
+# -- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --
+
+module ALX
+
+#==============================================================================
+#                                    CLASS
+#==============================================================================
+  
+# Class to check game roots.
+class RootChecker < EntryTransform
+
+#==============================================================================
+#                                   PUBLIC
+#==============================================================================
+
+  public
+
+  # Constructs a RootChecker.
+  def initialize
+    super(nil)
+  end
+
+end	# class RootChecker
+
+# -- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --
+
+end	# module ALX
+
+# -- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --
+
+if __FILE__ == $0
+  ALX::Main.call do
+    _rc = ALX::RootChecker.new
+    _rc.exec
+  end
+end
